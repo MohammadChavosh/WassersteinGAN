28b9da576846d09c4014fc3cc27f06df720ec74b
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2019-04-24 13:10:36 -0700

    progress bar added

--- extract_minutiae.py
+++ extract_minutiae.py
@@ -4,6 +4,8 @@ from fingerprints_dataset import get_fingerprint_images_list
 
 bashCommand = "/home/chavosh/NBIS/bin/mindtct {} /home/chavosh/NBIS_out/{}"
 images = get_fingerprint_images_list('/home/sadegh/Fingerprint_files/sd09/')
-for image in images:
+for idx, image in enumerate(images):
     process = subprocess.Popen(bashCommand.format(image, image.split('/')[-1][:-4]).split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    if idx % 1000 == 0:
+        print("{}/{} processed".format(idx + 1, len(images)))
